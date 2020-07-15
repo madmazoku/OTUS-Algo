2ade7a96c212ec032da702e9f118c1ce749bd945
Moskalyov Dmitry <madmazoku@gmail.com>
2020-07-15 23:28:04 +0300

    add Floyd-Warshall shortest path algorithm

--- lesson.18.cs/DijkstraShortestPath.cs
+++ lesson.18.cs/DijkstraShortestPath.cs
@@ -6,18 +6,21 @@ namespace lesson._18.cs
     class DijkstraShortestPath
     {
         AdjancenceVector<double> graph;
-        int startNode;
-        (int, double, double)[] data;
+        (int, double, double)[][] data;
 
-        public EdgeArray<double> Path(int endNode)
+        public EdgeArray<double> Path(int startNode, int endNode)
         {
             Build();
 
             NodeQueue<(int, int, double)> edges = new NodeQueue<(int, int, double)>();
+
+            (int, double, double)[] dataPath = data[startNode];
             int node = endNode;
             while (node != startNode)
             {
-                (int backNode, double weight, double sumWeight) = data[node];
+                (int backNode, double weight, _) = dataPath[node];
+                if (backNode == -1)
+                    throw new ArgumentException("unaccessable nodes");
                 edges.Enque((backNode, node, weight));
                 node = backNode;
             }
@@ -25,10 +28,9 @@ namespace lesson._18.cs
             return new EdgeArray<double>(graph.NodesCount, Util.ListToArray<(int, int, double)>(edges));
         }
 
-        public DijkstraShortestPath(AdjancenceVector<double> graph, int startNode)
+        public DijkstraShortestPath(AdjancenceVector<double> graph)
         {
             this.graph = graph;
-            this.startNode = startNode;
             data = null;
         }
 
@@ -37,39 +39,45 @@ namespace lesson._18.cs
             if (data != null)
                 return;
 
-            data = new (int, double, double)[graph.NodesCount];
-            Array.Fill(data, (-1, double.MaxValue, double.MaxValue));
-
-            int usedNodesCount = 0;
-            bool[] usedNodes = new bool[graph.NodesCount];
-
-            (int minNode, double minWeight) = (startNode, 0);
-            data[minNode] = (minNode, minWeight, minWeight);
-
-            while (minWeight < double.MaxValue)
+            data = new (int, double, double)[graph.NodesCount][];
+            for (int startNode = 0; startNode < graph.NodesCount; ++startNode)
             {
-                usedNodes[minNode] = true;
-                ++usedNodesCount;
 
-                (int, double)[] adjancentNodes = graph.Data[minNode];
-                for (int incendence = 0; incendence < adjancentNodes.Length; ++incendence)
+                (int, double, double)[] dataPath = data[startNode] = new (int, double, double)[graph.NodesCount];
+
+                Array.Fill(dataPath, (-1, double.MaxValue, double.MaxValue));
+
+                int usedNodesCount = 0;
+                bool[] usedNodes = new bool[graph.NodesCount];
+
+                (int minNode, double minWeight) = (startNode, 0);
+                dataPath[minNode] = (minNode, minWeight, minWeight);
+
+                while (minWeight < double.MaxValue)
                 {
-                    (int adjancentNode, double adjancentWeight) = adjancentNodes[incendence];
-                    if (adjancentWeight < 0)
-                        throw new ArgumentException("negative weight");
+                    usedNodes[minNode] = true;
+                    ++usedNodesCount;
 
-                    if (data[minNode].Item3 + adjancentWeight < data[adjancentNode].Item3)
-                        data[adjancentNode] = (minNode, adjancentWeight, data[minNode].Item3 + adjancentWeight);
+                    (int, double)[] adjancentNodes = graph.Data[minNode];
+                    for (int incendence = 0; incendence < adjancentNodes.Length; ++incendence)
+                    {
+                        (int adjancentNode, double adjancentWeight) = adjancentNodes[incendence];
+                        if (adjancentWeight < 0)
+                            throw new ArgumentException("negative weight");
+
+                        if (dataPath[minNode].Item3 + adjancentWeight < dataPath[adjancentNode].Item3)
+                            dataPath[adjancentNode] = (minNode, adjancentWeight, dataPath[minNode].Item3 + adjancentWeight);
+                    }
+
+                    (minNode, minWeight) = (-1, double.MaxValue);
+                    for (int node = 0; node < graph.NodesCount; ++node)
+                        if (!usedNodes[node] && dataPath[node].Item3 < minWeight)
+                            (minNode, minWeight) = (node, dataPath[node].Item3);
                 }
 
-                (minNode, minWeight) = (-1, double.MaxValue);
-                for (int node = 0; node < graph.NodesCount; ++node)
-                    if (!usedNodes[node] && data[node].Item3 < minWeight)
-                        (minNode, minWeight) = (node, data[node].Item3);
+                if (usedNodesCount < graph.NodesCount)
+                    throw new ArgumentException("unaccessable nodes");
             }
-
-            if (usedNodesCount < graph.NodesCount)
-                throw new ArgumentException("unaccessable nodes");
         }
     }
 }
---
+++ lesson.18.cs/FloydWarshallShortestPath.cs
@@ -0,0 +1,65 @@
+﻿using lesson._16.cs;
+using System;
+
+namespace lesson._18.cs
+{
+    class FloydWarshallShortestPath
+    {
+        AdjancenceVector<double> graph;
+        (int, double, double)[,] data;
+
+        public EdgeArray<double> Path(int startNode, int endNode)
+        {
+            Build();
+
+            NodeQueue<(int, int, double)> edges = new NodeQueue<(int, int, double)>();
+            int node = endNode;
+            while (node != startNode)
+            {
+                (int backNode, double weight, _) = data[startNode, node];
+                if (backNode == -1)
+                    throw new ArgumentException("unaccessable nodes");
+                edges.Enque((backNode, node, weight));
+                node = backNode;
+            }
+
+            return new EdgeArray<double>(graph.NodesCount, Util.ListToArray<(int, int, double)>(edges));
+        }
+
+        public FloydWarshallShortestPath(AdjancenceVector<double> graph)
+        {
+            this.graph = graph;
+            data = null;
+        }
+
+        void Build()
+        {
+            if (data != null)
+                return;
+
+            data = new (int, double, double)[graph.NodesCount, graph.NodesCount];
+            for (int node = 0; node < graph.NodesCount; ++node)
+            {
+                for (int adjancentNode = 0; adjancentNode < graph.NodesCount; ++adjancentNode)
+                    data[node, adjancentNode] = (-1, double.MaxValue, double.MaxValue);
+
+                (int, double)[] adjancentNodes = graph.Data[node];
+                for (int incendence = 0; incendence < adjancentNodes.Length; ++incendence)
+                {
+                    (int adjancentNode, double adjancentWeight) = adjancentNodes[incendence];
+                    data[node, adjancentNode] = (node, adjancentWeight, adjancentWeight);
+                }
+            }
+
+            for (int k = 0; k < graph.NodesCount; ++k)
+                for (int node = 0; node < graph.NodesCount; ++node)
+                    for (int adjancentNode = 0; adjancentNode < graph.NodesCount; ++adjancentNode)
+                        if (data[node, k].Item1 != -1 && data[k, adjancentNode].Item1 != -1 && data[node, adjancentNode].Item3 > data[node, k].Item3 + data[k, adjancentNode].Item3)
+                            data[node, adjancentNode] = (data[k, adjancentNode].Item1, data[k, adjancentNode].Item2, data[node, k].Item3 + data[k, adjancentNode].Item3);
+
+            for (int node = 0; node < graph.NodesCount; ++node)
+                if (data[node, node].Item3 < 0)
+                    throw new ArgumentException("negative loop");
+        }
+    }
+}
