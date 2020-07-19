88872922e540c9450f608abca5fd2d96d0f338b7
Moskalyov Dmitry <madmazoku@gmail.com>
2020-07-19 13:59:08 +0300

    add Bellman-Ford algorithm

--- lesson.18.cs/Program.cs
+++ lesson.18.cs/Program.cs
@@ -51,10 +51,35 @@ namespace lesson._18.cs
             Util.Print(edgeArray);
         }
 
+
+        static void TestBellmanFord()
+        {
+            Console.WriteLine(new String('-', Console.WindowWidth - 1));
+            Console.WriteLine("Test Bellman-Ford algorithm");
+
+            AdjancenceVector<double> adjancenceVector = new AdjancenceVector<double>(new (int, double)[][] {
+                /* 0 */new (int, double)[] { (1, 7.0), (2, 9.0), (5, 14.0) },
+                /* 1 */new (int, double)[] { (2, 10.0), (3, 15.0) },
+                /* 2 */new (int, double)[] { (3, 11.0), (5, 2.0) },
+                /* 3 */new (int, double)[] { (4, 6.0) },
+                /* 4 */new (int, double)[] { (5, 9.0) },
+                /* 5 */new (int, double)[] { },
+            });
+            adjancenceVector = adjancenceVector.Unidirectional();
+            Console.WriteLine("adjancenceVector");
+            Util.Print(adjancenceVector);
+
+            (int startNode, int endNode) = (0, 4);
+            Console.WriteLine($"Shortest path from {startNode} to {endNode}");
+            EdgeArray<double> edgeArray = (new BellmanFordShortestPath(adjancenceVector)).Path(startNode, endNode);
+            Util.Print(edgeArray);
+        }
+
         static void Main(string[] args)
         {
             TestDijkstra();
             TestFloydWarshall();
+            TestBellmanFord();
         }
     }
 }
---
+++ lesson.18.cs/ShortestPath/BellmanFordShortestPath.cs
@@ -0,0 +1,70 @@
+﻿using lesson._16.cs;
+using System;
+
+namespace lesson._18.cs
+{
+    class BellmanFordShortestPath
+    {
+        AdjancenceVector<double> graph;
+        (int, double, double)[][] data;
+
+        public EdgeArray<double> Path(int startNode, int endNode)
+        {
+            Build();
+
+            NodeQueue<(int, int, double)> edges = new NodeQueue<(int, int, double)>();
+            int node = endNode;
+            while (node != startNode)
+            {
+                (int backNode, double weight, _) = data[startNode][node];
+                if (backNode == -1)
+                    throw new ArgumentException("unaccessable nodes");
+                edges.Enque((backNode, node, weight));
+                node = backNode;
+            }
+
+            return new EdgeArray<double>(graph.NodesCount, Util.ListToArray<(int, int, double)>(edges));
+        }
+
+        public BellmanFordShortestPath(AdjancenceVector<double> graph)
+        {
+            this.graph = graph;
+            data = null;
+        }
+
+        void Build()
+        {
+            if (data != null)
+                return;
+
+            EdgeArray<double> edgeArray = new EdgeArray<double>(graph);
+
+            data = new (int, double, double)[graph.NodesCount][];
+            for (int startNode = 0; startNode < graph.NodesCount; ++startNode)
+            {
+                (int, double, double)[] dataPath = data[startNode] = new (int, double, double)[graph.NodesCount];
+                Array.Fill(dataPath, (-1, double.MaxValue, double.MaxValue));
+                dataPath[startNode] = (startNode, 0, 0);
+
+                bool relaxed;
+                int realxedCount = 0;
+                do
+                {
+                    if (realxedCount++ == graph.NodesCount)
+                        throw new ArgumentException("negative loop");
+                    relaxed = false;
+                    for (int edge = 0; edge < edgeArray.Data.Length; ++edge)
+                    {
+                        (int from, int to, double weight) = edgeArray.Data[edge];
+                        if (dataPath[from].Item1 != -1)
+                            if (dataPath[to].Item3 > dataPath[from].Item3 + weight)
+                            {
+                                dataPath[to] = (from, weight, dataPath[from].Item3 + weight);
+                                relaxed = true;
+                            }
+                    }
+                } while (relaxed);
+            }
+        }
+    }
+}
