dd3122fd28b8d7d8846d13203656f2167223d5b5
Moskalyov Dmitry <madmazoku@gmail.com>
2020-07-26 18:24:42 +0300

    lesson 19

---
+++ lesson.19.cs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lesson._19.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задача коммивояжёра. A*");
+        }
+    }
+}
