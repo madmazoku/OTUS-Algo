22a8f0981bb74c50d6eecbf7493bce614c9bd871
Moskalyov Dmitry <madmazoku@gmail.com>
2020-09-10 21:08:33 +0300

    add lesson 30

---
+++ lesson.30.cs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lesson._30.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Blum filter!");
+        }
+    }
+}
